6fdc321d9aac298cf7acfca4ffe46f02a4f19108
muhammadhassannaseer1244-coder <mhassannaseerahmed@gmail.com>
2026-04-27 13:04:26 +0500

    remove anchor tag

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -99,9 +99,7 @@ export default function Login() {
                             </div>
 
                             <div className="text-sm">
-                                <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
-                                    Forgot your password?
-                                </a>
+                               
                             </div>
                         </div>
 
