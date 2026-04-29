0b2843eb67ea5842d8f29255fbd384056959bf96
muhammadhassannaseer1244-coder <mhassannaseerahmed@gmail.com>
2026-04-29 16:56:50 +0500

    api issue fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: "https://shop-flow-backend-three.vercel.app/api"
 });
 
 // Add a request interceptor to include auth token
