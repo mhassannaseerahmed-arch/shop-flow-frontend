0e178369221f87ee19ace875ab86076e34c173be
muhammadhassannaseer1244-coder <mhassannaseerahmed@gmail.com>
2026-04-29 16:46:15 +0500

    api issue fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-        baseURL: process.env.REACT_APP_API_URL || 'api'
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 // Add a request interceptor to include auth token
